26b52278c382c00d119fce120d75ffe538d0829d
Johann Feser <johannfeser@gmail.com>
2019-02-18 13:09:12 +0100

    progessbar

--- app/src/main/java/de/unikassel/chefcoders/codecampkitchen/ui/LoginActivity.java
+++ app/src/main/java/de/unikassel/chefcoders/codecampkitchen/ui/LoginActivity.java
@@ -1,10 +1,11 @@
 package de.unikassel.chefcoders.codecampkitchen.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 
 import de.unikassel.chefcoders.codecampkitchen.R;
 
@@ -14,6 +15,7 @@ public class LoginActivity extends AppCompatActivity
     private EditText editTextName;
     private EditText editTextEmail;
     private Button buttonLogin;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -24,11 +26,13 @@ public class LoginActivity extends AppCompatActivity
         editTextName = findViewById(R.id.editTextName);
         editTextEmail = findViewById(R.id.editTextEmail);
         buttonLogin = findViewById(R.id.buttonLogin);
+        progressBar = findViewById(R.id.progressBar);
     }
 
 
     public void loginClick(View v)
     {
+        progressBar.setVisibility(View.VISIBLE);
         String name = editTextName.getText().toString();
         String email = editTextEmail.getText().toString();
 
