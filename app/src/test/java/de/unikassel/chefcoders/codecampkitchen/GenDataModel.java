a4dae525a34ee72678c43eedbf4f3c4c3fbe5718
Adrian Kunz <clashsoft@hotmail.com>
2019-02-18 11:39:29 +0100

    GenDataModel
    - Added the GenDataModel class.

---
+++ app/src/test/java/de/unikassel/chefcoders/codecampkitchen/GenDataModel.java
@@ -0,0 +1,50 @@
+package de.unikassel.chefcoders.codecampkitchen;
+
+import org.fulib.Fulib;
+import org.fulib.builder.ClassBuilder;
+import org.fulib.builder.ClassModelBuilder;
+import org.fulib.classmodel.ClassModel;
+
+import static org.fulib.builder.ClassModelBuilder.MANY;
+import static org.fulib.builder.ClassModelBuilder.ONE;
+
+public class GenDataModel
+{
+	public static void main(String[] args)
+	{
+		final ClassModelBuilder mb = Fulib.classModelBuilder("de.unikassel.chefcoders.codecampkitchen.model");
+
+		// classes
+
+		final ClassBuilder user = mb.buildClass("User");
+		user.buildAttribute("_id", "String");
+		user.buildAttribute("credit", "long");
+		user.buildAttribute("role", "String");
+		user.buildAttribute("created", "String");
+		user.buildAttribute("name", "String");
+		user.buildAttribute("mail", "String");
+
+		final ClassBuilder item = mb.buildClass("Item");
+		item.buildAttribute("_id", "String");
+		item.buildAttribute("name", "String");
+		item.buildAttribute("price", "double");
+		item.buildAttribute("amount", "int");
+		item.buildAttribute("kind", "String");
+
+		final ClassBuilder purchase = mb.buildClass("Purchase");
+		purchase.buildAttribute("_id", "String");
+		purchase.buildAttribute("created", "String");
+		purchase.buildAttribute("amount", "int");
+
+		// associations
+
+		purchase.buildAssociation(user, "user", ONE, "purchases", MANY);
+		purchase.buildAttribute("item", "Item"); // should be uni-directional
+
+		// generate model
+
+		final ClassModel model = mb.getClassModel();
+
+		Fulib.generator().generate(model);
+	}
+}
