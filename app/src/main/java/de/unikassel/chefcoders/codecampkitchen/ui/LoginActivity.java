0a6ebc3d7f46980376c28e5fa5f68898dd7854cd
Johann Feser <johannfeser@gmail.com>
2019-02-18 12:53:41 +0100

    Update LoginActivity.java

--- app/src/main/java/de/unikassel/chefcoders/codecampkitchen/ui/LoginActivity.java
+++ app/src/main/java/de/unikassel/chefcoders/codecampkitchen/ui/LoginActivity.java
@@ -19,7 +19,6 @@ public class LoginActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setTheme(R.style.LoginTheme);
         setContentView(R.layout.activity_login);
 
         editTextName = findViewById(R.id.editTextName);
