2b43eff89cbe7f7af8192ead544ca0e9b5b1e425
sst97 <sst97@gmx.net>
2019-02-18 11:24:38 +0100

    Interface for HTTP-Connection

---
+++ app/src/main/java/HttpConnection.java
@@ -0,0 +1,9 @@
+public interface HttpConnection {
+    public String get(String url);
+
+    public void post(String url, String jsonBody);
+
+    public void put(String url, String jsonBody);
+
+    public void delete(String url);
+}
