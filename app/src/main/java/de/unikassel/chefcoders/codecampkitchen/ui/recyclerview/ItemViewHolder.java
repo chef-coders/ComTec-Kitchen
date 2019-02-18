e4c48b05f714a553986148c4425372461b65dcfd
JanNB97 <j.bingemann@gmx.de>
2019-02-18 15:19:20 +0100

    relative layout in item_view

--- app/src/main/java/de/unikassel/chefcoders/codecampkitchen/ui/recyclerview/ItemViewHolder.java
+++ app/src/main/java/de/unikassel/chefcoders/codecampkitchen/ui/recyclerview/ItemViewHolder.java
@@ -1,5 +1,6 @@
 package de.unikassel.chefcoders.codecampkitchen.ui.recyclerview;
 
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
@@ -9,12 +10,15 @@ import de.unikassel.chefcoders.codecampkitchen.R;
 
 public class ItemViewHolder extends RecyclerView.ViewHolder
 {
-	public TextView textView;
+	private View itemView;
+	private TextView textView;
 
 	public ItemViewHolder(View itemView)
 	{
 		super(itemView);
-		this.textView = itemView.findViewById(R.id.text);
+		this.itemView = itemView;
+		this.textView = itemView.findViewById(R.id.title);
+		this.itemView.setBackgroundColor(Color.parseColor("#e5e5e5"));
 	}
 
 	public void showText(String s)
