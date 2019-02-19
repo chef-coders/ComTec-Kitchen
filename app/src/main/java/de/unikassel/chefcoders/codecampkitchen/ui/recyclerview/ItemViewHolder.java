ec0395df28b7adaea6a57ad32e5c76920b7596f2
JanNB97 <j.bingemann@gmx.de>
2019-02-19 14:15:42 +0100

    ItemHolder show price and amount

--- app/src/main/java/de/unikassel/chefcoders/codecampkitchen/ui/recyclerview/ItemViewHolder.java
+++ app/src/main/java/de/unikassel/chefcoders/codecampkitchen/ui/recyclerview/ItemViewHolder.java
@@ -1,7 +1,6 @@
 package de.unikassel.chefcoders.codecampkitchen.ui.recyclerview;
 
 import android.graphics.Color;
-import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
@@ -12,18 +11,49 @@ import de.unikassel.chefcoders.codecampkitchen.model.Item;
 public class ItemViewHolder extends RecyclerView.ViewHolder
 {
 	private View itemView;
-	private TextView textView;
+	private TextView titleTextView;
+	private TextView prizeTextView;
+	private TextView amountTextView;
 
 	public ItemViewHolder(View itemView)
 	{
 		super(itemView);
 		this.itemView = itemView;
-		this.textView = itemView.findViewById(R.id.title);
+		this.titleTextView = itemView.findViewById(R.id.item_title);
+		this.prizeTextView = itemView.findViewById(R.id.item_prize);
+		this.amountTextView = itemView.findViewById(R.id.item_amount);
+
 		this.itemView.setBackgroundColor(Color.parseColor("#e5e5e5"));
 	}
 
 	public void show(Item item)
 	{
-		this.textView.setText(item.getName());
+		if(item == null)
+		{
+			return;
+		}
+
+		if(item.getName() != null)
+		{
+			this.titleTextView.setText(item.getName());
+		}
+
+		if(item.getPrice() > 0.0)
+		{
+			this.prizeTextView.setText(String.format("%.2f €", item.getPrice()));
+		}
+		else
+		{
+			this.prizeTextView.setText("-");
+		}
+
+		if(item.getAmount() != 0)
+		{
+			this.amountTextView.setText(item.getAmount() + " available");
+		}
+		else
+		{
+			this.amountTextView.setText("Not available");
+		}
 	}
 }
