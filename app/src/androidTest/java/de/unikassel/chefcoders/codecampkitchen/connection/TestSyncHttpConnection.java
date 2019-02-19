ed26cae8b86f0642be8c5dee870ef1eb687ab457
Michael Prasil <michaprx@gmail.com>
2019-02-19 10:08:17 +0100

    The sync http connection works.

--- app/src/androidTest/java/de/unikassel/chefcoders/codecampkitchen/connection/TestSyncHttpConnection.java
+++ app/src/androidTest/java/de/unikassel/chefcoders/codecampkitchen/connection/TestSyncHttpConnection.java
@@ -1,24 +1,24 @@
 package de.unikassel.chefcoders.codecampkitchen.connection;
 
+import android.support.test.runner.AndroidJUnit4;
+
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import de.unikassel.chefcoders.codecampkitchen.communication.HttpConnection;
 import de.unikassel.chefcoders.codecampkitchen.communication.SyncHttpConnection;
 import de.unikassel.chefcoders.codecampkitchen.communication.SyncHttpMethodException;
 
+@RunWith(AndroidJUnit4.class)
 public class TestSyncHttpConnection {
 	@Test
-	public void testGetMethod() {
-		try {
-			HttpConnection connection = new SyncHttpConnection();
-			String result = connection.get("/");
-			Assert.assertNotNull(result);
-			Assert.assertEquals("{\"version\":\"1.0\",\"name\":\"Kitchen Management\"}", result);
-		} catch (SyncHttpMethodException ex) {
-			Assert.assertNull(null);
-		} catch (Exception ex) {
-			Assert.fail(ex.getMessage());
-		}
+	public void testGetMethod() throws SyncHttpMethodException {
+		HttpConnection connection = new SyncHttpConnection();
+
+		String result = connection.get("");
+
+		Assert.assertNotNull(result);
+		Assert.assertEquals("{\"version\":\"1.0\",\"name\":\"Kitchen Management\"}", result);
 	}
 }
--- app/src/main/java/de/unikassel/chefcoders/codecampkitchen/communication/SyncHttpConnection.java
+++ app/src/main/java/de/unikassel/chefcoders/codecampkitchen/communication/SyncHttpConnection.java
@@ -15,68 +15,101 @@ public class SyncHttpConnection implements HttpConnection {
 			client = new SyncHttpClient();
 		}
 
-		public String get(String url) throws SyncHttpMethodException {
+		public String get(String relativeUrl) throws SyncHttpMethodException {
 			try {
-				client.get(createUrl(url), null, new JsonHttpResponseHandler() {
+				String url = createUrl(relativeUrl);
+				client.get(url, null, new JsonHttpResponseHandler() {
 					@Override
 					public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
 						lastResult = response.toString();
 					}
+					@Override
+					public void onFailure(int statusCode, Header[] headers, Throwable e, JSONObject errorResponse) {
+						lastResult = null;
+					}
 				});
 			} catch (Exception ex) {
+				lastResult = null;
 				throw new SyncHttpMethodException(ex, HttpMethod.Get);
+			}
+			if (lastResult == null) {
+				throw new SyncHttpMethodException(null, HttpMethod.Get);
 			}
 			return lastResult;
 		}
 
-		public String post(String url, String jsonBody, Map<String, String> headers) throws SyncHttpMethodException {
+		public String post(String relativeUrl, String jsonBody, Map<String, String> headers) throws SyncHttpMethodException {
 			try {
 				RequestParams parameters = new RequestParams();
 				for (Map.Entry<String, String> entry : headers.entrySet()) {
 					parameters.add(entry.getKey(), entry.getValue());
 				}
 
+				String url = createUrl(relativeUrl);
 				client.post(createUrl(url), parameters, new JsonHttpResponseHandler() {
 					@Override
 					public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
 						lastResult = response.toString();
 					}
+					@Override
+					public void onFailure(int statusCode, Header[] headers, Throwable e, JSONObject errorResponse) {
+						lastResult = null;
+					}
 				});
 			} catch (Exception ex) {
 				throw new SyncHttpMethodException(ex, HttpMethod.Post);
 			}
+			if (lastResult == null) {
+				throw new SyncHttpMethodException(null, HttpMethod.Get);
+			}
 			return lastResult;
 		}
 
-		public String put(String url, String jsonBody, Map<String, String> headers) throws SyncHttpMethodException {
+		public String put(String relativeUrl, String jsonBody, Map<String, String> headers) throws SyncHttpMethodException {
 			try {
 				RequestParams parameters = new RequestParams();
 				for (Map.Entry<String, String> entry : headers.entrySet()) {
 					parameters.add(entry.getKey(), entry.getValue());
 				}
 
+				String url = createUrl(relativeUrl);
 				client.put(createUrl(url), parameters, new JsonHttpResponseHandler() {
 					@Override
 					public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
 						lastResult = response.toString();
 					}
+					@Override
+					public void onFailure(int statusCode, Header[] headers, Throwable e, JSONObject errorResponse) {
+						lastResult = null;
+					}
 				});
 			} catch (Exception ex) {
 				throw new SyncHttpMethodException(ex, HttpMethod.Put);
 			}
+			if (lastResult == null) {
+				throw new SyncHttpMethodException(null, HttpMethod.Get);
+			}
 			return lastResult;
 		}
 
-		public String delete(String url) throws SyncHttpMethodException {
+		public String delete(String relativeUrl) throws SyncHttpMethodException {
 			try {
+				String url = createUrl(relativeUrl);
 				client.delete(createUrl(url), null, new JsonHttpResponseHandler() {
 					@Override
 					public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
 						lastResult = response.toString();
 					}
+					@Override
+					public void onFailure(int statusCode, Header[] headers, Throwable e, JSONObject errorResponse) {
+						lastResult = null;
+					}
 				});
 			} catch (Exception ex) {
 				throw new SyncHttpMethodException(ex, HttpMethod.Delete);
+			}
+			if (lastResult == null) {
+				throw new SyncHttpMethodException(null, HttpMethod.Get);
 			}
 			return lastResult;
 		}
