345c5e59b1a4ca6db8e8043d081c3a1b8574762c
Adrian Kunz <clashsoft@hotmail.com>
2019-02-19 09:42:52 +0100

    Clean up Connection tests

--- app/src/test/java/de/unikassel/chefcoders/codecampkitchen/connection/TestSyncHttpConnection.java
+++ app/src/test/java/de/unikassel/chefcoders/codecampkitchen/connection/TestSyncHttpConnection.java
@@ -1,24 +1,21 @@
 package de.unikassel.chefcoders.codecampkitchen.connection;
 
-import org.junit.Assert;
-import org.junit.Test;
-
 import de.unikassel.chefcoders.codecampkitchen.communication.HttpConnection;
 import de.unikassel.chefcoders.codecampkitchen.communication.SyncHttpConnection;
 import de.unikassel.chefcoders.codecampkitchen.communication.SyncHttpMethodException;
+import org.junit.Test;
 
-public class TestSyncHttpConnection {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class TestSyncHttpConnection
+{
 	@Test
-	public void testGetMethod() {
-		try {
-			HttpConnection connection = new SyncHttpConnection();
-			String result = connection.get("/");
-			Assert.assertNotNull(result);
-			Assert.assertEquals("{\"version\":\"1.0\",\"name\":\"Kitchen Management\"}", result);
-		} catch (SyncHttpMethodException ex) {
-			Assert.assertNull(null);
-		} catch (Exception ex) {
-			Assert.fail(ex.getMessage());
-		}
+	public void testGetMethod() throws SyncHttpMethodException
+	{
+		HttpConnection connection = new SyncHttpConnection();
+		String result = connection.get("/");
+		assertNotNull(result);
+		assertEquals("{\"version\":\"1.0\",\"name\":\"Kitchen Management\"}", result);
 	}
 }
