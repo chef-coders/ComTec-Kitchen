eb147304806bbe242ade2be78789a43cafad6751
Johann Feser <johannfeser@gmail.com>
2019-02-19 14:44:29 +0100

    Update MainActivity.java
    check if user is logged in

--- app/src/main/java/de/unikassel/chefcoders/codecampkitchen/MainActivity.java
+++ app/src/main/java/de/unikassel/chefcoders/codecampkitchen/MainActivity.java
@@ -17,6 +17,7 @@ import android.view.MenuItem;
 import de.unikassel.chefcoders.codecampkitchen.logic.KitchenManager;
 import de.unikassel.chefcoders.codecampkitchen.ui.AllItemsFragment;
 import de.unikassel.chefcoders.codecampkitchen.ui.BarcodeScannerActivity;
+import de.unikassel.chefcoders.codecampkitchen.ui.LoginActivity;
 import de.unikassel.chefcoders.codecampkitchen.ui.MyPurchasesFragment;
 
 public class MainActivity extends AppCompatActivity
@@ -32,10 +33,13 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        boolean isLoggedIn = kitchenManager.tryLogin(this);
+        if (!isLoggedIn) {
+            startLogin();
+        }
 
 
-
-        if(savedInstanceState==null){
+        if (savedInstanceState == null) {
             this.initFragment();
         }
 
@@ -43,7 +47,13 @@ public class MainActivity extends AppCompatActivity
         this.initNavDrawer();
     }
 
-    private void initFragment(){
+    private void startLogin()
+    {
+        startActivity(new Intent(this, LoginActivity.class));
+    }
+
+    private void initFragment()
+    {
         changeFragment(new AllItemsFragment());
     }
 
@@ -106,7 +116,7 @@ public class MainActivity extends AppCompatActivity
         FragmentTransaction transaction = getSupportFragmentManager()
                 .beginTransaction();
 
-        if(fragment instanceof AllItemsFragment){
+        if (fragment instanceof AllItemsFragment) {
             transaction.setCustomAnimations(
                     R.anim.slide_in_left,
                     R.anim.slide_out_right
