01e14a70059b3e658342db68d5fac2ea0b3be4d7
Adrian Kunz <clashsoft@hotmail.com>
2019-02-19 11:50:28 +0100

    Clean up JsonTranslator

--- app/src/main/java/de/unikassel/chefcoders/codecampkitchen/model/JsonTranslator.java
+++ app/src/main/java/de/unikassel/chefcoders/codecampkitchen/model/JsonTranslator.java
@@ -15,14 +15,27 @@ public class JsonTranslator
 
 	// =============== Static Methods ===============
 
+	public static <T> String toJson(T object, Class<T> type)
+	{
+		return gson.toJson(object);
+	}
+
+	public static <T> T fromJson(String json, Class<T> type)
+	{
+		return gson.fromJson(json, type);
+	}
+
 	// @formatter:off
-	public static String toJson(User user) { return gson.toJson(user); }
-	public static User toUser(String json) { return gson.fromJson(json, User.class); }
+	public static String toJson(User user) { return toJson(user, User.class); }
+	// public static void toUser(String json, User target) { fromJson(json, target); }
+	public static User toUser(String json) { return fromJson(json, User.class); }
 
-	public static String toJson(Item item) { return gson.toJson(item); }
-	public static Item toItem(String json) { return gson.fromJson(json, Item.class); }
+	public static String toJson(Item item) { return toJson(item, Item.class); }
+	// public static void toItem(String json, Item target) { fromJson(json, target); }
+	public static Item toItem(String json) { return fromJson(json, Item.class); }
 
-	public static String toJson(Purchase purchase) { return gson.toJson(purchase); }
-	public static Purchase toPurchase(String json) { return gson.fromJson(json, Purchase.class); }
+	public static String toJson(Purchase purchase) { return toJson(purchase, Purchase.class); }
+	// public static void toPurchase(String json, Purchase target) { fromJson(json, target); }
+	public static Purchase toPurchase(String json) { return fromJson(json, Purchase.class); }
 	// @formatter:on
 }
