380a067b2f647781b732b6fdab61ac11c3661355
Johann Feser <johannfeser@gmail.com>
2019-02-19 14:11:54 +0100

    Update MainActivity.java
    KitchenManager

--- app/src/main/java/de/unikassel/chefcoders/codecampkitchen/MainActivity.java
+++ app/src/main/java/de/unikassel/chefcoders/codecampkitchen/MainActivity.java
@@ -14,6 +14,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import de.unikassel.chefcoders.codecampkitchen.logic.KitchenManager;
 import de.unikassel.chefcoders.codecampkitchen.ui.AllItemsFragment;
 import de.unikassel.chefcoders.codecampkitchen.ui.BarcodeScannerActivity;
 import de.unikassel.chefcoders.codecampkitchen.ui.MyPurchasesFragment;
@@ -23,11 +24,16 @@ public class MainActivity extends AppCompatActivity
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
 
+    public static KitchenManager kitchenManager
+            = KitchenManager.create();
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
         if(savedInstanceState==null){
             this.initFragment();
