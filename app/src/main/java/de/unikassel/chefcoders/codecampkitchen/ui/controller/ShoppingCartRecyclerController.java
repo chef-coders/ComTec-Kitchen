b297bbb993c816f84d6270d9fcddaede3ff952e4
Adrian Kunz <clashsoft@hotmail.com>
2019-02-20 15:54:01 +0100

    Add ShoppingCartRecyclerController

---
+++ app/src/main/java/de/unikassel/chefcoders/codecampkitchen/ui/controller/ShoppingCartRecyclerController.java
@@ -0,0 +1,59 @@
+package de.unikassel.chefcoders.codecampkitchen.ui.controller;
+
+import android.view.View;
+import de.unikassel.chefcoders.codecampkitchen.MainActivity;
+import de.unikassel.chefcoders.codecampkitchen.model.Item;
+import de.unikassel.chefcoders.codecampkitchen.model.Purchase;
+import de.unikassel.chefcoders.codecampkitchen.ui.recyclerview.RowViewHolder;
+
+import java.util.List;
+
+public class ShoppingCartRecyclerController implements RecyclerController<RowViewHolder>
+{
+	private List<Purchase> shoppingCart;
+
+	@Override
+	public int getSections()
+	{
+		return 1;
+	}
+
+	@Override
+	public int getItems(int section)
+	{
+		return this.shoppingCart.size();
+	}
+
+	@Override
+	public String getHeader(int section)
+	{
+		return "";
+	}
+
+	@Override
+	public void refresh()
+	{
+		this.shoppingCart = MainActivity.kitchenManager.getCart();
+	}
+
+	@Override
+	public RowViewHolder create(View view)
+	{
+		return new RowViewHolder(view);
+	}
+
+	@Override
+	public void populate(RowViewHolder v, int section, int itemIndex)
+	{
+		final Purchase purchase = this.shoppingCart.get(itemIndex);
+		final Item item = MainActivity.kitchenManager.getItemById(purchase.getItem_id());
+
+		Populator.populate(v, item);
+	}
+
+	@Override
+	public boolean onClick(int section, int item)
+	{
+		return false;
+	}
+}
