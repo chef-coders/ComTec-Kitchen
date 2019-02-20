f2f6903057e35f3969b26ffee3c8a731649aff08
Johann Feser <johannfeser@gmail.com>
2019-02-20 16:05:37 +0100

    confirm purchases

--- app/src/main/java/de/unikassel/chefcoders/codecampkitchen/ui/ConfirmPurchasesFragment.java
+++ app/src/main/java/de/unikassel/chefcoders/codecampkitchen/ui/ConfirmPurchasesFragment.java
@@ -2,19 +2,28 @@ package de.unikassel.chefcoders.codecampkitchen.ui;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 
+import de.unikassel.chefcoders.codecampkitchen.MainActivity;
 import de.unikassel.chefcoders.codecampkitchen.R;
+import de.unikassel.chefcoders.codecampkitchen.ui.controller.ItemRecyclerController;
+import de.unikassel.chefcoders.codecampkitchen.ui.controller.ShoppingCartRecyclerController;
+import de.unikassel.chefcoders.codecampkitchen.ui.recyclerview.GeneralRecyclerView;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ConfirmPurchasesFragment extends KitchenFragment
+public class ConfirmPurchasesFragment extends KitchenFragment implements GeneralRecyclerView.RecViewEventHandler
 {
+
+    private FloatingActionButton floatingActionButton;
 
     public ConfirmPurchasesFragment()
     {
@@ -27,8 +36,52 @@ public class ConfirmPurchasesFragment extends KitchenFragment
                              Bundle savedInstanceState)
     {
         View view = inflater.inflate(R.layout.fragment_confirm_purchases, container, false);
-
+        this.initFloatingActionButton(view);
+        this.initRecyclerView(view);
         return view;
     }
 
+    private void initFloatingActionButton(View allItemsView)
+    {
+        floatingActionButton = allItemsView.findViewById(R.id.buyItemButton);
+        floatingActionButton.setOnClickListener(v -> {
+            MainActivity mainActivity = (MainActivity) getActivity();
+            if (mainActivity != null) {
+
+            }
+        });
+    }
+
+    private void initRecyclerView(View allItemsView)
+    {
+        new GeneralRecyclerView(allItemsView.findViewById(R.id.allItemsRecView),
+                new ShoppingCartRecyclerController(),
+                allItemsView.findViewById(R.id.allItemsSwipeRefreshLayout),
+                this);
+    }
+
+
+    @Override
+    public void handleRecViewLoadFinished()
+    {
+
+    }
+
+    @Override
+    public void handleRecViewScrolledDown(@NonNull RecyclerView recyclerView, int dx, int dy)
+    {
+        this.floatingActionButton.hide();
+    }
+
+    @Override
+    public void handleRecViewScrolledUp(@NonNull RecyclerView recyclerView, int dx, int dy)
+    {
+        this.floatingActionButton.show();
+    }
+
+    @Override
+    public void handleRecViewItemTouched(View view, int position)
+    {
+        // TODO - Handle touch
+    }
 }
