2c7404fac3f89e3c6522a94a54e18b7b5dcfbc72
Michael Prasil <michaprx@gmail.com>
2019-02-20 13:20:39 +0100

    Refurbished the barcode activities.

--- app/src/main/java/de/unikassel/chefcoders/codecampkitchen/ui/barcodes/CreateItemActivity.java
+++ app/src/main/java/de/unikassel/chefcoders/codecampkitchen/ui/barcodes/CreateItemActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import de.unikassel.chefcoders.codecampkitchen.MainActivity;
 import de.unikassel.chefcoders.codecampkitchen.R;
@@ -14,6 +15,7 @@ public class CreateItemActivity extends AppCompatActivity
 {
 	private String barcode;
 
+	private TextView barcodeValue;
 	private EditText nameText;
 	private EditText priceText;
 	private EditText amountText;
@@ -27,10 +29,13 @@ public class CreateItemActivity extends AppCompatActivity
 
 		barcode = (String) getIntent().getExtras().get("barcode");
 
+		this.barcodeValue = findViewById(R.id.barcodeValueView);
 		this.nameText = findViewById(R.id.nameText);
 		this.priceText = findViewById(R.id.priceText);
 		this.amountText = findViewById(R.id.amountText);
 		this.kindText = findViewById(R.id.kindText);
+
+		this.barcodeValue.setText(barcode);
 	}
 
 	public void onCreate(View view) {
