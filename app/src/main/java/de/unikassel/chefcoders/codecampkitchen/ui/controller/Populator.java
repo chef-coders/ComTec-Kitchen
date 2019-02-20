ad9ce819189b171dd3e7a88aec8f0e66dc6ab8ca
Adrian Kunz <clashsoft@hotmail.com>
2019-02-20 15:29:27 +0100

    Move populate logic to Populator
    - Moved the populate logic from ItemRecyclerController to Populator.

---
+++ app/src/main/java/de/unikassel/chefcoders/codecampkitchen/ui/controller/Populator.java
@@ -0,0 +1,42 @@
+package de.unikassel.chefcoders.codecampkitchen.ui.controller;
+
+import de.unikassel.chefcoders.codecampkitchen.MainActivity;
+import de.unikassel.chefcoders.codecampkitchen.R;
+import de.unikassel.chefcoders.codecampkitchen.model.Item;
+import de.unikassel.chefcoders.codecampkitchen.ui.recyclerview.RowViewHolder;
+
+public class Populator
+{
+
+	static void populate(RowViewHolder v, Item item)
+	{
+		if (item.getName() != null)
+		{
+			v.titleTextView.setText(item.getName());
+		}
+
+		if (item.getPrice() > 0.0)
+		{
+			v.prizeTextView.setText(v.itemView.getContext().getString(R.string.item_price, item.getPrice()));
+		}
+		else
+		{
+			v.prizeTextView.setText("-");
+		}
+
+		if (item.getAmount() != 0)
+		{
+			String amountAvailable = v.itemView.getContext()
+			                                   .getString(R.string.item_amount_available, item.getAmount());
+			v.amountTextView.setText(amountAvailable);
+		}
+		else
+		{
+			v.amountTextView.setText(R.string.item_amount_not_available);
+		}
+
+		final int numInCart = MainActivity.kitchenManager.getCartAmount(item);
+		final String numInCartText = v.itemView.getContext().getString(R.string.item_amount, numInCart);
+		v.numSelectedTextView.setText(numInCartText);
+	}
+}
