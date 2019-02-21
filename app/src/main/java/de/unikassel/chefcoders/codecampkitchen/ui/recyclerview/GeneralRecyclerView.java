a358f1e5a79d3ebfcb0a5d8499d08994c18dea31
JanNB97 <j.bingemann@gmx.de>
2019-02-21 11:08:49 +0100

    RecyclerView: red backgroundcolor on swipe

--- app/src/main/java/de/unikassel/chefcoders/codecampkitchen/ui/recyclerview/GeneralRecyclerView.java
+++ app/src/main/java/de/unikassel/chefcoders/codecampkitchen/ui/recyclerview/GeneralRecyclerView.java
@@ -1,6 +1,11 @@
 package de.unikassel.chefcoders.codecampkitchen.ui.recyclerview;
 
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -65,9 +70,41 @@ public class GeneralRecyclerView
 
 		ItemTouchHelper.SimpleCallback itemTouchHelperCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT)
 		{
+			private Drawable icon = new ColorDrawable(Color.RED);
+			private ColorDrawable background = new ColorDrawable(Color.RED);
+
 			@Override
 			public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder viewHolder1)
 			{ return false; }
+
+			@Override
+			public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive)
+			{
+				super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+				View itemView = viewHolder.itemView;
+				int backgroundCornerOffset = 20;
+
+				int iconMargin = (itemView.getHeight() - icon.getIntrinsicHeight()) / 2;
+				int iconTop = itemView.getTop() + (itemView.getHeight() - icon.getIntrinsicHeight() / 2);
+				int iconBottom = iconTop + icon.getIntrinsicHeight();
+
+				if(dX < 0)
+				{
+					// swiping to the left
+					int iconLeft = itemView.getRight() - iconMargin - icon.getIntrinsicWidth();
+					int iconRight = itemView.getRight() - iconMargin;
+					icon.setBounds(iconLeft, iconTop, iconRight, iconBottom);
+
+					this.background.setBounds(itemView.getRight() + ((int) dX) - backgroundCornerOffset,
+							itemView.getTop(), itemView.getRight(), itemView.getBottom());
+				}
+				else
+				{
+					this.background.setBounds(0, 0, 0, 0);
+				}
+				this.background.draw(c);
+				this.icon.draw(c);
+			}
 
 			@Override
 			public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction)
