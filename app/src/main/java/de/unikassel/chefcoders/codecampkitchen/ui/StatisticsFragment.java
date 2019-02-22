d9c2df983c101a3dc5bf56368eed676958ce9668
Michael Prasil <michaprx@gmail.com>
2019-02-22 14:31:16 +0100

    Implemented basic statistics.

--- app/src/main/java/de/unikassel/chefcoders/codecampkitchen/ui/StatisticsFragment.java
+++ app/src/main/java/de/unikassel/chefcoders/codecampkitchen/ui/StatisticsFragment.java
@@ -1,17 +1,19 @@
 package de.unikassel.chefcoders.codecampkitchen.ui;
 
-import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.jjoe64.graphview.GraphView;
-import com.jjoe64.graphview.LegendRenderer;
-import com.jjoe64.graphview.ValueDependentColor;
-import com.jjoe64.graphview.helper.StaticLabelsFormatter;
-import com.jjoe64.graphview.series.BarGraphSeries;
-import com.jjoe64.graphview.series.DataPoint;
+import com.github.mikephil.charting.charts.HorizontalBarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
 
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
@@ -41,7 +43,7 @@ public class StatisticsFragment extends KitchenFragment
 	}
 
 	private void initAmountOfBoughtItemsChart(View allItemsView) {
-		GraphView graph = (GraphView) allItemsView.findViewById(R.id.graph);
+		HorizontalBarChart horizontalBarChart = (HorizontalBarChart) allItemsView.findViewById(R.id.chart);
 
 		LinkedHashMap<Item, Integer> boughtItems = new LinkedHashMap<>();
 		for (Purchase purchase : this.purchases) {
@@ -56,26 +58,53 @@ public class StatisticsFragment extends KitchenFragment
 		}
 
 		int index = 0;
-		List<DataPoint> dataPoints = new ArrayList<>();
-		List<String> names = new ArrayList<>();
+		ArrayList<IBarDataSet> dataSets = new ArrayList<>();
+		ArrayList<String> names = new ArrayList<>();
 		for (Map.Entry<Item, Integer> entry : boughtItems.entrySet()) {
-			dataPoints.add(new DataPoint(index++, entry.getValue()));
 			String name = entry.getKey().getName();
-			int lastIndex = name.length() >= 3 ? 3 : name.length();
-			names.add(entry.getKey().getName().substring(0, lastIndex));
+			names.add(name);
+
+			BarEntry value = new BarEntry(index++, entry.getValue());
+
+			ArrayList<BarEntry> record = new ArrayList<>();
+			record.add(value);
+
+			BarDataSet x = new BarDataSet(record, name);
+			dataSets.add(x);
 		}
 
-		StaticLabelsFormatter staticLabelsFormatter = new StaticLabelsFormatter(graph);
-		staticLabelsFormatter.setHorizontalLabels(names.toArray(new String[0]));
-		graph.getGridLabelRenderer().setLabelFormatter(staticLabelsFormatter);
+		XAxis xAxis = horizontalBarChart.getXAxis();
+		xAxis.setLabelCount(names.size());
+		xAxis.setValueFormatter(new IndexAxisValueFormatter(names));
+		xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+		xAxis.setDrawAxisLine(true);
+		xAxis.setDrawGridLines(false);
+		xAxis.setEnabled(true);
 
-		BarGraphSeries<DataPoint> series = new BarGraphSeries<>(dataPoints.toArray(new DataPoint[0]));
-		graph.addSeries(series);
+		YAxis yAxis = horizontalBarChart.getAxisLeft();
+		yAxis.setDrawAxisLine(true);
+		yAxis.setDrawGridLines(true);
+		yAxis.setAxisMinimum(0f);
+		yAxis.setAxisMinimum(0f);
 
-		series.setValueDependentColor(data -> Color.rgb((int) data.getX()*255/4, (int) Math.abs(data.getY()*255/6), 100));
-		series.setSpacing(50);
-		series.setDataWidth(1.5);
-		series.setDrawValuesOnTop(true);
-		series.setValuesOnTopColor(Color.RED);
+		YAxis yAxisRight = horizontalBarChart.getAxisRight();
+		yAxisRight.setDrawAxisLine(true);
+		yAxisRight.setDrawGridLines(false);
+
+		horizontalBarChart.setFitBars(true);
+		horizontalBarChart.animateXY(2500, 2500);
+
+		BarData data = new BarData(dataSets);
+
+		horizontalBarChart.setData(data);
+		horizontalBarChart.getDescription().setEnabled(false);
+		horizontalBarChart.getLegend().setEnabled(false);
+		horizontalBarChart.invalidate();
+	}
+
+	@Override
+	protected void updateToolbar(Toolbar toolbar)
+	{
+		toolbar.setTitle(R.string.statistics);
 	}
 }
