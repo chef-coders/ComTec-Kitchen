bac8ac9f94dbc42122266f4adfe99bbd44f1cb86
Adrian Kunz <clashsoft@hotmail.com>
2019-02-22 20:26:19 +0100

    Clean up JsonTranslator
    - Fixed formatting.
    - Implemented List parsing methods more efficiently.

--- app/src/main/java/de/unikassel/chefcoders/codecampkitchen/model/JsonTranslator.java
+++ app/src/main/java/de/unikassel/chefcoders/codecampkitchen/model/JsonTranslator.java
@@ -1,10 +1,8 @@
 package de.unikassel.chefcoders.codecampkitchen.model;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import com.google.gson.reflect.TypeToken;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class JsonTranslator
@@ -12,11 +10,6 @@ public class JsonTranslator
 	// =============== Static Fields ===============
 
 	private static final Gson gson = new Gson();
-
-	static
-	{
-		// Gson adapters...
-	}
 
 	// =============== Static Methods ===============
 
@@ -30,26 +23,23 @@ public class JsonTranslator
 		return gson.fromJson(json, type);
 	}
 
+	public static <T> List<T> fromJsons(String jsonArray, Class<T[]> type)
+	{
+		return Arrays.asList(gson.fromJson(jsonArray, type));
+	}
+
 	// @formatter:off
+	// --------------- Users ---------------
 	public static String toJson(User user) { return toJson(user, User.class); }
-
-	// public static void toUser(String json, User target) { fromJson(json, target); }
 	public static User toUser(String json) { return fromJson(json, User.class); }
-
-	public static List<User> toUsers(String json) { return gson.fromJson(json, new TypeToken<List<User>>() {}.getType()); }
-
+	public static List<User> toUsers(String json) { return fromJsons(json, User[].class); }
+	// --------------- Items ---------------
 	public static String toJson(Item item) { return toJson(item, Item.class); }
-
-	// public static void toItem(String json, Item target) { fromJson(json, target); }
 	public static Item toItem(String json) { return fromJson(json, Item.class); }
-
-	public static List<Item> toItems(String json) { return gson.fromJson(json, new TypeToken<List<Item>>() {}.getType()); }
-
+	public static List<Item> toItems(String json) { return fromJsons(json, Item[].class); }
+	// --------------- Purchases ---------------
 	public static String toJson(Purchase purchase) { return toJson(purchase, Purchase.class); }
-
-	// public static void toPurchase(String json, Purchase target) { fromJson(json, target); }
 	public static Purchase toPurchase(String json) { return fromJson(json, Purchase.class); }
-
-	public static List<Purchase> toPurchases(String json) { return gson.fromJson(json, new TypeToken<List<Purchase>>() {}.getType()); }
+	public static List<Purchase> toPurchases(String json) { return fromJsons(json, Purchase[].class); }
 	// @formatter:on
 }
