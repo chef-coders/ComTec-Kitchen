55ee972d2873984131f4025949908015244bc969
Johann Feser <johannfeser@gmail.com>
2019-02-22 09:42:39 +0100

    Update AllUserFragment.java
    add get user by section and item call

--- app/src/main/java/de/unikassel/chefcoders/codecampkitchen/ui/AllUserFragment.java
+++ app/src/main/java/de/unikassel/chefcoders/codecampkitchen/ui/AllUserFragment.java
@@ -11,7 +11,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
 
+import de.unikassel.chefcoders.codecampkitchen.MainActivity;
 import de.unikassel.chefcoders.codecampkitchen.R;
+import de.unikassel.chefcoders.codecampkitchen.model.User;
 import de.unikassel.chefcoders.codecampkitchen.ui.controller.UserRecyclerController;
 import de.unikassel.chefcoders.codecampkitchen.ui.recyclerview.GeneralRecyclerView;
 
@@ -75,7 +77,11 @@ public class AllUserFragment extends KitchenFragment implements GeneralRecyclerV
 	@Override
 	public void onClick(int section, int item)
 	{
-
+		MainActivity mainActivity = (MainActivity) getActivity();
+		User user = MainActivity.kitchenManager.getUser(section, item);
+		if (mainActivity != null) {
+			mainActivity.changeFragment(EditUserFragment.newInstance(user.get_id()));
+		}
 	}
 
 	@Override
