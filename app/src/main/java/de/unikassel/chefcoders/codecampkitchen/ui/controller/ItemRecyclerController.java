03d909179b3e628f4bc7ae3f9c3bf6e9390b40e1
Adrian Kunz <clashsoft@hotmail.com>
2019-02-22 11:11:41 +0100

    Move Item Edit Click functionality to ItemRecyclerController

--- app/src/main/java/de/unikassel/chefcoders/codecampkitchen/ui/controller/ItemRecyclerController.java
+++ app/src/main/java/de/unikassel/chefcoders/codecampkitchen/ui/controller/ItemRecyclerController.java
@@ -1,9 +1,11 @@
 package de.unikassel.chefcoders.codecampkitchen.ui.controller;
 
-import android.support.v7.widget.RecyclerView;
+import android.content.Context;
+import android.content.Intent;
 import android.view.View;
 import de.unikassel.chefcoders.codecampkitchen.MainActivity;
 import de.unikassel.chefcoders.codecampkitchen.model.Item;
+import de.unikassel.chefcoders.codecampkitchen.ui.barcodes.EditItemActivity;
 import de.unikassel.chefcoders.codecampkitchen.ui.recyclerview.RowViewHolder;
 
 public class ItemRecyclerController extends GroupedRecyclerController<Item, RowViewHolder>
@@ -30,12 +32,20 @@ public class ItemRecyclerController extends GroupedRecyclerController<Item, RowV
 	@Override
 	public boolean onClick(RowViewHolder v, int section, int itemIndex)
 	{
+		final Item item = this.get(section, itemIndex);
 		if (!MainActivity.editMode)
 		{
-			return MainActivity.kitchenManager.addToCart(this.get(section, itemIndex)) > 0;
+			return MainActivity.kitchenManager.addToCart(item) > 0;
 		}
 
-		return false;
+		final View itemView = v.itemView;
+		itemView.post(() -> {
+			final Context context = itemView.getContext();
+			Intent intent = new Intent(context, EditItemActivity.class);
+			intent.putExtra("itemId", item.get_id());
+			context.startActivity(intent);
+		});
+		return true;
 	}
 
 	@Override
