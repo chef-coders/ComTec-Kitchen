32919fa569e0b387c6c95079db9639fcfa4ec085
Adrian Kunz <clashsoft@hotmail.com>
2019-02-22 20:05:52 +0100

    Implement SimpleAsyncTask using ResultAsyncTask

--- app/src/main/java/de/unikassel/chefcoders/codecampkitchen/ui/multithreading/SimpleAsyncTask.java
+++ app/src/main/java/de/unikassel/chefcoders/codecampkitchen/ui/multithreading/SimpleAsyncTask.java
@@ -1,52 +1,19 @@
 package de.unikassel.chefcoders.codecampkitchen.ui.multithreading;
 
 import android.content.Context;
-import android.os.AsyncTask;
-import android.widget.Toast;
-import de.unikassel.chefcoders.codecampkitchen.communication.errorhandling.HttpConnectionException;
 
-public class SimpleAsyncTask extends AsyncTask<Void, Void, Void>
+public abstract class SimpleAsyncTask
 {
-	private final Context  context;
-	private final Runnable backgroundRunnable;
-	private final Runnable postExecuteRunnable;
-
-	private String errorMessage;
-
-	private SimpleAsyncTask(Context context, Runnable backgroundRunnable, Runnable postExecuteRunnable)
+	private SimpleAsyncTask()
 	{
-		this.context = context;
-		this.backgroundRunnable = backgroundRunnable;
-		this.postExecuteRunnable = postExecuteRunnable;
+		// no instances
 	}
 
 	public static void execute(Context context, Runnable backgroundRunnable, Runnable postExecuteRunnable)
 	{
-		new SimpleAsyncTask(context, backgroundRunnable, postExecuteRunnable).execute();
-	}
-
-	@Override
-	protected Void doInBackground(Void... voids)
-	{
-		try
-		{
-			this.backgroundRunnable.run();
-		}
-		catch (HttpConnectionException ex)
-		{
-			this.errorMessage = ex.smallErrorMessage();
-		}
-		return null;
-	}
-
-	@Override
-	protected void onPostExecute(Void v)
-	{
-		if (this.errorMessage != null)
-		{
-			Toast.makeText(this.context, this.errorMessage, Toast.LENGTH_LONG).show();
-			return;
-		}
-		this.postExecuteRunnable.run();
+		ResultAsyncTask.execute(context, () -> {
+			backgroundRunnable.run();
+			return null;
+		}, ignored -> postExecuteRunnable.run());
 	}
 }
